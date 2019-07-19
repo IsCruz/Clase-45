44e5db00265ecc66e994e64c3920ee483da6c2d6
IsCruz <israelcruzibarra@gmail.com>
2019-07-19 16:27:16 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
-const PORT = 4000; 
+const PORT = 4000;
 
-let Todo = require('./models/todo.model'); 
+let Todo = require('./models/todo.model');
 app.use(cors());
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
 const connection = mongoose.connection; connection.once('open', function () {
     console.log("MongoDB database connection established successfully");
 })
 
-todoRoutes.get('/' , (req, res) => {
+todoRoutes.get('/', (req, res) => {
     Todo.find(function (err, todos) {
         if (err) {
             console.log(err);
@@ -28,7 +28,6 @@ todoRoutes.get('/' , (req, res) => {
 
 todoRoutes.post('/delete', (req, res) => {
     let description = req.body.todo_description
-    console.log(description)
     Todo.deleteOne({ todo_description: description }, (err) => {
         if (err) return next(err);
         res.send('Deleted successfully!');
@@ -44,9 +43,9 @@ todoRoutes.post('/add', (req, res) => {
         .catch(err => {
             res.status(400).send('adding new todo failed');
         });
-}); 
+});
 
-app.use('/todos', todoRoutes); 
+app.use('/todos', todoRoutes);
 
 app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
